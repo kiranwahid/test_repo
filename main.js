8af185aec89904cac9c6a0f8b3e15679dbe2493e
kiranwahid <kiranwahid666@gmail.com>
2024-05-11 00:42:55 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log('hello world');
+let a = 20;
+let b = 5;
+let sum = a + b;
+console.log(sum);
+function myfun(a, b) {
+    return a + b;
+}
+let result = myfun('kiran ', 'wahid');
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,13 @@
+console.log('hello world');
+
+let a:number = 20;
+let b:number = 5;
+let sum:number = a + b;
+console.log(sum);
+
+function myfun(a:string,b:string){
+    return a +  b
+}
+
+let result =  myfun('kiran ','wahid')
+console.log(result);
